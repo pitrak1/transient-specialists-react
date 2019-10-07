c92d98669688c883bc85a97fa4afbef25d3ecabf
Nick Pitrak <pitrak1@gmail.com>
2019-10-07 18:20:49 -0500

    add basic conversion scripts from azure to aws postgres

---
+++ bin/parseEquipment.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+echo "NOTE: filenames cannot have any spaces"
+
+if [ -z "$1" ]
+then
+    echo "no input filename provided"
+    exit 1
+fi
+
+if [ -z "$2" ]
+then
+    echo "no output filename provided"
+    exit 1
+fi
+
+INPUT="$1"
+OUTPUT="$2"
+LINENUMBER=1
+echo "INSERT INTO Equipment (id, serial_number, notes, cal_company, cal_due, type_id, model_id) VALUES" > $OUTPUT
+while read LINE
+do
+  if [ $LINENUMBER -ge 2 ]
+  then
+    if [ $LINENUMBER -ge 3 ]
+    then
+      echo "," >> $OUTPUT
+    fi
+
+    IFS=',' read -ra COLUMNS <<< "$LINE"
+
+    printf "(" >> $OUTPUT
+
+    ID=${COLUMNS[0]//\"/}
+    printf "$ID" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    SERIALNUMBER=${COLUMNS[1]//\"/\'}
+    printf "$SERIALNUMBER" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    NOTES=${COLUMNS[2]}
+    if [ "$NOTES" == "\"null\"" ]
+    then
+      printf "${NOTES//\"/}" >> $OUTPUT
+    else
+      printf "${NOTES//\"/\'}" >> $OUTPUT
+    fi
+    printf ', ' >> $OUTPUT
+
+    CALCOMPANY=${COLUMNS[4]}
+    if [ "$CALCOMPANY" == "\"null\"" ]
+    then
+      printf "${CALCOMPANY//\"/}" >> $OUTPUT
+    else
+      printf "${CALCOMPANY//\"/\'}" >> $OUTPUT
+    fi
+    printf ', ' >> $OUTPUT
+
+    CALDUE=${COLUMNS[5]}
+    if [ "$CALDUE" == "\"null\"" ]
+    then
+      printf "${CALDUE//\"/}" >> $OUTPUT
+    else
+      printf "${CALDUE//\"/\'}" >> $OUTPUT
+    fi
+    printf ', ' >> $OUTPUT
+
+    TYPEID=${COLUMNS[6]//\"/}
+    printf "$TYPEID" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    MODELID=${COLUMNS[7]//\"/}
+    printf "$MODELID" >> $OUTPUT
+
+    printf ")" >> $OUTPUT
+  fi
+  LINENUMBER=$((LINENUMBER+1))
+done < $INPUT
+
+printf ";" >> $OUTPUT
+
---
+++ bin/parseEvents.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+then
+    echo "no input filename provided"
+    exit 1
+fi
+
+if [ -z "$2" ]
+then
+    echo "no output filename provided"
+    exit 1
+fi
+
+INPUT="$1"
+OUTPUT="$2"
+LINENUMBER=1
+echo "INSERT INTO Events (id, status, updated_at, equipment_id, company_notes, start_date, end_date) VALUES" > $OUTPUT
+while read LINE
+do
+  if [ $LINENUMBER -ge 2 ]
+  then
+    if [ $LINENUMBER -ge 3 ]
+    then
+      echo "," >> $OUTPUT
+    fi
+
+    IFS=',' read -ra COLUMNS <<< "$LINE"
+
+    printf "(" >> $OUTPUT
+
+    ID=${COLUMNS[0]//\"/}
+    printf "$ID" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    STATUS=${COLUMNS[1]//\"/\'}
+    printf "$STATUS" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    UPDATEDAT=${COLUMNS[2]//\"/\'}
+    printf "$UPDATEDAT" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    EQUIPMENTID=${COLUMNS[3]//\"/}
+    printf "$EQUIPMENTID" >> $OUTPUT
+    printf ', ' >> $OUTPUT
+
+    COMPANYNOTES=${COLUMNS[4]}
+    if [ "$COMPANYNOTES" == "\"null\"" ]
+    then
+      printf "${COMPANYNOTES//\"/}" >> $OUTPUT
+    else
+      printf "${COMPANYNOTES//\"/\'}" >> $OUTPUT
+    fi
+    printf ', ' >> $OUTPUT
+
+    STARTDATE=${COLUMNS[5]}
+    if [ "$STARTDATE" == "\"null\"" ]
+    then
+      printf "${STARTDATE//\"/}" >> $OUTPUT
+    else
+      printf "${STARTDATE//\"/\'}" >> $OUTPUT
+    fi
+    printf ', ' >> $OUTPUT
+
+    ENDDATE=${COLUMNS[6]}
+    if [ "$ENDDATE" == "\"null\"" ]
+    then
+      printf "${ENDDATE//\"/}" >> $OUTPUT
+    else
+      printf "${ENDDATE//\"/\'}" >> $OUTPUT
+    fi
+
+    printf ")" >> $OUTPUT
+
+  fi
+  LINENUMBER=$((LINENUMBER+1))
+done < $INPUT
+
+printf ";" >> $OUTPUT
+
